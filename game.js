ee635ed8541cef653364a740833a9434cacab673
berkay oren <benberkayoren@gmail.com>
2022-09-13 20:31:47 +0300

    Commit

--- game.js
+++ game.js
@@ -2,3 +2,32 @@
 //*  GUESS MY NUMBER
 //*  Game Logic
 //*======================================
+
+//? 1/100 arasi rastgele bir sayi tut.
+
+const randomNumber = Math.round(Math.random() * 100);
+console.log(randomNumber);
+
+//* CheckBtn basildiğinda kontrolleri yap
+document.querySelector(".check-btn").addEventListener("click", () => {
+  const guessInput = document.querySelector(".guess-input");
+});
+
+//! eger rastgele sayi ==input.value
+//? tebrikler bildiniz.
+//? background = green
+//? eger score > topScore
+//?   topScore = score
+//? secret_number = gorunur
+
+//! değilse
+// eger score > 0
+//!   score = score -1
+//?   eger rastgele < input.value
+//?     DECREASE
+//? degilse
+//?     INCREASE
+//! degilse
+//? uzgunuz kaybettiniz
+
+//* againBtn basildiginda kontrolleri yap
