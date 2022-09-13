b77debe6939c841d88df918a619a3ab11dfadbc7
berkay oren <benberkayoren@gmail.com>
2022-09-13 21:36:15 +0300

    commit

--- game.js
+++ game.js
@@ -8,20 +8,46 @@
 const randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
 
+//? Variables
+let score = 10;
+let topScore = 0;
+
 //* CheckBtn basildiğinda kontrolleri yap
 document.querySelector(".check-btn").addEventListener("click", () => {
   const guessInput = Number(document.querySelector(".guess-input").value);
   const msg = document.querySelector(".msg");
+  const body = document.querySelector("body");
   //! eger input girilmediyse uyari ver
 
   if (!guessInput) {
     msg.innerText = "Please Enter a Number";
+    //! eger rastgele sayi == input.value
   } else if (randomNumber === guessInput) {
-    msg.innerText = "Congrats You Won";
+    msg.innerHTML = `Congrats you won <i class="fa-solid fa-face-grin-hearts fa-2x"></i>`;
+    // document.querySelector("body").style.background = "green";
+    body.className = "bg-success";
+    if (score > topScore) {
+      topScore = score;
+      document.querySelector(".top-score").textContent = topScore;
+    }
+
+    document.querySelector(".secret-number").textContent = randomNumber;
+  } else {
+    score--;
+    if (score > 0) {
+      guessInput > randomNumber
+        ? (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-down fa-2x"></i> DECREASE `)
+        : (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-up fa-2x"></i> INCREASE `);
+    } else {
+      msg.innerHTML = `You Lost <i class="fa-regular fa-face-sad-tear fa-2x"></i>`;
+      body.className = "bg-danger";
+      document.querySelector(".check-btn").disabled = true;
+    }
+
+    document.querySelector(".score").textContent = score;
   }
 });
 
-//! eger rastgele sayi ==input.value
 //? tebrikler bildiniz.
 //? background = green
 //? eger score > topScore
